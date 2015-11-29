4927fbd18ad20b89b198fa1b92103a4b613ae9d3
liangran <252118071@qq.com>
2015-11-29 10:55:45 +0800

    add pages

--- js/routes.js
+++ js/routes.js
@@ -9,14 +9,35 @@ angular.module('MyApp').config(['$stateProvider', '$urlRouterProvider',
         $urlRouterProvider.otherwise('/');
 
         // Application routes
-        $stateProvider
-            .state('index', {
-                url: '/',
-                templateUrl: 'templates/dashboard.html'
-            })
-            .state('tables', {
-                url: '/tables',
-                templateUrl: 'templates/tables.html'
-            });
+		$stateProvider
+			.state('index', {
+				url : '/',
+				templateUrl : 'templates/home.html'
+			}).state('create', {
+				url : '/create',
+				templateUrl : 'templates/create.html'
+			}).state('trust', {
+				url : '/trust',
+				templateUrl : 'templates/trust.html'
+			}).state('taobao', {
+				url : '/taobao',
+				templateUrl : 'templates/taobao.html'
+			}).state('withdraw', {
+				url : '/withdraw',
+				templateUrl : 'templates/withdraw.html'
+			}).state('trade', {
+				url : '/trade',
+				templateUrl : 'templates/trade.html'
+			}).state('manual', {
+				url : '/manual',
+				templateUrl : 'templates/manual.html'
+			});
     }
 ]);
+
+angular.module('MyApp').run(function($rootScope) {
+	$rootScope.$on('$stateChangeSuccess', function() {
+		console.log('$stateChangeSuccess');
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+	});
+});
